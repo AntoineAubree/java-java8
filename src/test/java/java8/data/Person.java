45639dc2beb0b03a53cc730705a6e7e346ce61e2
DESKTOP-J4CVPG5\Antoine <antoine.aubree@protonmail.com>
2021-03-24 16:54:19 +0100

    add exercices 1 et 2

--- src/test/java/java8/data/Person.java
+++ src/test/java/java8/data/Person.java
@@ -2,50 +2,65 @@ package java8.data;
 
 public class Person {
 
-    private String firstname;
-    private String lastname;
-    private Integer age;
-    private String password;
+	private String firstname;
+	private String lastname;
+	private Integer age;
+	private String password;
 
-    public Person() {
-    }
+	public Person() {
+	}
 
-    public Person(String firstname, String lastname, Integer age, String password) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.age = age;
-        this.password = password;
-    }
+	public Person(String firstname, String lastname, Integer age, String password) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.age = age;
+		this.password = password;
+	}
 
-    public String getFirstname() {
-        return firstname;
-    }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Person [firstname=");
+		builder.append(firstname);
+		builder.append(", lastname=");
+		builder.append(lastname);
+		builder.append(", age=");
+		builder.append(age);
+		builder.append(", password=");
+		builder.append(password);
+		builder.append("]");
+		return builder.toString();
+	}
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
+	public String getFirstname() {
+		return firstname;
+	}
 
-    public String getLastname() {
-        return lastname;
-    }
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
+	public String getLastname() {
+		return lastname;
+	}
 
-    public Integer getAge() {
-        return age;
-    }
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+	public Integer getAge() {
+		return age;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
