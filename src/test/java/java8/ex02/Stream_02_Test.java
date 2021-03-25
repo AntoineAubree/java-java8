c4fdce8e8646066d0d7c8588963c7b9c706ed970
DESKTOP-J4CVPG5\Antoine <antoine.aubree@protonmail.com>
2021-03-25 16:38:11 +0100

    add exo stream

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -1,17 +1,17 @@
 package java8.ex02;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
 import java8.data.Data;
 import java8.data.domain.Customer;
 import java8.data.domain.Order;
-import java8.data.domain.Pizza;
-import org.junit.Test;
-
-import java.util.IntSummaryStatistics;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
 
 /**
  * Exercice 02 - Transformation
@@ -24,7 +24,7 @@ public class Stream_02_Test {
         List<Order> orders = new Data().getOrders();
 
         // Trouver la liste des clients ayant déjà passés une commande
-        List<Customer> result = null;
+        Set<Customer> result = orders.stream().map(order -> order.getCustomer()).collect(Collectors.toSet());
 
         assertThat(result, hasSize(2));
     }
